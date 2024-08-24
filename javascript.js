497576e22f9f7d0c06e3589fb4fc581e9df07e69
Brandon Lin <brandonlin9716@gmail.com>
2024-08-24 14:11:59 -0700

    Add logic for creating initial gameboard

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+function createCell() {
+
+    let value = 0;
+}
+
+function createGameboard() {
+    
+    let gameboard = [];
+    const ROWS = 3;
+    const COLUMNS = 3;
+
+    for (let i = 0; i < ROWS; i++) {
+        gameboard[i] = [];
+        for (let j = 0; j < COLUMNS; j++) {
+            gameboard[i].push(createCell());
+        }
+    }
+}
