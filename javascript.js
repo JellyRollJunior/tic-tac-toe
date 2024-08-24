2e988ab7b91ea8a9bdf3ad7933ebfdd9a6a257f5
Brandon Lin <brandonlin9716@gmail.com>
2024-08-24 14:19:12 -0700

    Add functions to retrieve and change cell value

--- javascript.js
+++ javascript.js
@@ -1,18 +1,27 @@
 function createCell() {
-
     let value = 0;
+
+    const getValue = () => value;
+
+    const addToken = (player) => {
+        value = player;
+    };
+
+    return { getValue, addToken };
 }
 
 function createGameboard() {
-    
     let gameboard = [];
     const ROWS = 3;
     const COLUMNS = 3;
 
+    // create initial board
     for (let i = 0; i < ROWS; i++) {
         gameboard[i] = [];
         for (let j = 0; j < COLUMNS; j++) {
             gameboard[i].push(createCell());
         }
     }
+
+    return { gameboard };
 }
