32cfceb173394985dbbaf70e2dffaf4e9b64571b
Brandon Lin <brandonlin9716@gmail.com>
2024-08-24 23:09:18 -0700

    Add function to verify tic-tac-toe winner

--- javascript.js
+++ javascript.js
@@ -30,7 +30,6 @@ function createGameboard() {
 
     const placeToken = (row, column, playerToken) => {
         const cell = gameboard[row][column];
-        // check if cell is occupied
         if (cell.getValue() === 0) {
             cell.addToken(playerToken);
         } else {
@@ -38,7 +37,50 @@ function createGameboard() {
         }
     };
 
-    return { gameboard, printGameboard, placeToken };
+    const verifyWinner = (playerToken) => {
+        // check row for win
+        for (let i = 0; i < ROWS; i++) {
+            for (let j = 0; j < COLUMNS; j++) {
+                if (gameboard[i][j].getValue() !== playerToken) {
+                    break;
+                } else if (j === COLUMNS - 1) {
+                    return true;
+                }
+            }
+        }
+        // check columns for win
+        for (let i = 0; i < COLUMNS; i++) {
+            for (let j = 0; i < ROWS; j++) {
+                if (gameboard[j][i].getValue() !== playerToken) {
+                    break;
+                } else if (i === ROWS - 1) {
+                    return true;
+                }
+            }
+        }
+        // check diagonal for win
+        for (let i = 0; i < ROWS; i++) {
+            if (gameboard[i][i] !== playerToken) {
+                break;
+            } else if (i === ROWS - 1) {
+                return true;
+            }
+        }
+        // check reverse diagonal
+        let reverseDiagonalColumn = 0;
+        for (let i = ROWS - 1; i < 0; i--) {
+            if (gameboard[i][reverseDiagonalColumn++] !== playerToken) {
+                break;
+            } else if (i === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    return { printGameboard, placeToken , verifyWinner};
 }
 
-let testBoard = createGameboard();
+let test = createGameboard();
+test.placeToken(0, 0, 2);
+test.verifyWinner(2);
